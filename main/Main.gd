74160e4713568573e297536ac484899d37666faf
jaguarshadow <starbucknakari@gmail.com>
2021-07-13 20:44:37 -0500

    Space Rocks Godot Tutorial

---
+++ main/Main.gd
@@ -0,0 +1,99 @@
+extends Node2D
+
+export (PackedScene) var Rock
+export (PackedScene) var Enemy
+
+var screensize
+var level = 0
+var score = 0
+var playing = false
+
+func _ready():
+	randomize()
+	screensize = get_viewport().get_visible_rect().size
+	$Player.screensize = screensize
+	for i in range(3):
+		spawn_rock(3)
+		
+func spawn_rock(size, pos=null, vel=null):
+	if !pos:
+		$RockPath/RockSpawn.set_offset(randi())
+		pos = $RockPath/RockSpawn.position
+	if !vel:
+		vel = Vector2(1, 0).rotated(rand_range(0,2*PI)) * rand_range(100, 150)
+	var r = Rock.instance()
+	r.screensize = screensize
+	r.start(pos, vel, size)
+	$Rocks.add_child(r)
+	r.connect('exploded', self, '_on_Rock_exploded')
+
+func _on_Player_shoot(bullet, pos, dir):
+	var b = bullet.instance()
+	b.start(pos, dir)
+	add_child(b)
+	
+func _on_Rock_exploded(size, radius, pos, vel):
+	$ExplodeSound.play()
+	score += 1
+	$HUD.update_score(score)
+	if size <= 1:
+		return
+	for offset in [-1, 1]:
+		var dir = (pos - $Player.position).normalized().tangent() * offset
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1, newpos, newvel)
+
+func new_game():
+	$Music.play()
+	for rock in $Rocks.get_children():
+		rock.queue_free()
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.update_shield($Player.max_shield)
+	$Player.start()
+	$HUD.show_message("Get Ready!")
+	yield($HUD/MessageTimer, "timeout")
+	playing = true
+	new_level()
+	
+func new_level():
+	$LevelupSound.play()
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in range(level):
+		spawn_rock(3)
+	$EnemyTimer.wait_time = rand_range(5, 10)
+	$EnemyTimer.start()
+
+func _process(delta):
+	if not playing:
+		return
+	if playing and $Rocks.get_child_count() == 0:
+		new_level()
+		
+func game_over():
+	$Music.stop()
+	playing = false
+	$HUD.game_over()
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if not playing:
+			return
+		get_tree().paused = not get_tree().paused
+		if get_tree().paused:
+			$HUD/MessageLabel.text = "Paused"
+			$HUD/MessageLabel.show()
+		else:
+			$HUD/MessageLabel.text = ""
+			$HUD/MessageLabel.hide()
+
+func _on_EnemyTimer_timeout():
+	var e = Enemy.instance()
+	add_child(e)
+	e.target = $Player
+	e.connect('shoot', self, '_on_Player_shoot')
+	$EnemyTimer.wait_time = rand_range(20, 40)
+	$EnemyTimer.start()
---
+++ player/Player.gd
@@ -0,0 +1,139 @@
+extends RigidBody2D
+
+enum {INIT, ALIVE, INVULNERABLE, DEAD}
+var state = null
+
+var screensize = Vector2()
+
+signal shoot
+signal lives_changed
+signal dead
+signal shield_changed
+
+var lives = 0 setget set_lives
+var shield = 0 setget set_shield
+
+export (PackedScene) var Bullet
+export (float) var fire_rate
+export (int) var max_shield
+export (float) var shield_regen
+
+var can_shoot = true
+
+func _ready():
+	change_state(INIT)
+	screensize = get_viewport().get_visible_rect().size
+	$GunTimer.wait_time = fire_rate
+	
+func change_state(new_state):
+	match new_state:
+		INIT:
+			$CollisionShape2D.disabled = true
+			$Sprite.modulate.a = 0.5
+		ALIVE:
+			$CollisionShape2D.disabled = false
+			$Sprite.modulate.a = 1.0
+		INVULNERABLE:
+			$CollisionShape2D.disabled = true
+			$Sprite.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
+		DEAD:
+			$EngineSound.stop()
+			$CollisionShape2D.disabled = true
+			$Sprite.hide()
+			linear_velocity = Vector2()
+			emit_signal("dead")
+	state = new_state
+	
+export (int) var engine_power
+export (int) var spin_power
+	
+var thrust = Vector2()
+var rotation_dir = 0
+	
+func _process(delta):
+	self.shield += shield_regen * delta
+	get_input()
+
+func get_input():
+	$Exhaust.emitting = false
+	thrust = Vector2()
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		$Exhaust.emitting = true
+		thrust = Vector2(engine_power, 0)
+		if not $EngineSound.playing:
+			$EngineSound.play()
+	else:
+		$EngineSound.stop()
+	rotation_dir = 0
+	if Input.is_action_pressed("rotate_right"):
+		rotation_dir += 1
+	if Input.is_action_pressed("rotate_left"):
+		rotation_dir -= 1
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+
+func _integrate_forces(physics_state):
+	set_applied_force(thrust.rotated(rotation))
+	set_applied_torque(spin_power * rotation_dir)
+	var xform = physics_state.get_transform()
+	if xform.origin.x > screensize.x:
+		xform.origin.x = 0
+	if xform.origin.x < 0:
+		xform.origin.x = screensize.x
+	if xform.origin.y > screensize.y:
+		xform.origin.y = 0
+	if xform.origin.y < 0:
+		xform.origin.y = screensize.y
+	physics_state.set_transform(xform)
+
+func shoot():
+	if state == INVULNERABLE:
+		return
+	emit_signal("shoot", Bullet, $Muzzle.global_position, rotation)
+	can_shoot = false
+	$LaserSound.play()
+	$GunTimer.start()
+	
+func _on_GunTimer_timeout():
+	can_shoot = true
+
+func set_lives(value):
+	lives = value
+	emit_signal("lives_changed", lives)
+	self.shield = max_shield
+	
+func start():
+	$Sprite.show()
+	self.lives = 3
+	self.shield = max_shield
+	change_state(ALIVE)
+	
+func _on_InvulnerabilityTimer_timeout():
+	change_state(ALIVE)
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	$Explosion.hide()
+
+func _on_Player_body_entered(body):
+	if body.is_in_group('rocks'):
+		body.explode()
+		$ExplodeSound.play()
+		$Explosion.show()
+		$Explosion/AnimationPlayer.play("explosion")
+		self.shield -= body.size * 10
+		if lives < 0 and self.shield <= 0:
+			change_state(DEAD)
+		else:
+			change_state(INVULNERABLE)
+			
+func set_shield(value):
+	if value > max_shield:
+		value = max_shield
+	shield = value
+	emit_signal("shield_changed", shield/max_shield * 100)
+	if shield <= 0:
+		self.lives -= 1
+		
